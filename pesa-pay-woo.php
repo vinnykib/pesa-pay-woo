dab0729cd3bfe6955122e53c337d894f53458c6e
Vinny <knvinny@gmail.com>
2021-02-25 15:48:23 +0300

    First commit after adding plugin main file

---
+++ pesa-pay-woo.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Mpesa Payment Gateway
+ * Plugin URI: https://vinny.com
+ * Author: Vinny
+ * Author URI: https://vinny.com
+ * Description: Mobile mpesa payment woocommerce.
+ * Version: 1.0.0
+ * License: GPL3
+ * License URL: 
+ * text-domain: pesa-pay-woo
+ * 
+ * 
+ */
